e28d927c3551c25707839fd934a545773fa4d6bd
YuriSukhorukov <yuri.sukhorukov.spb@gmail.com>
2018-03-19 12:34:10 +0300

    Уменьшение кода в методах поиска доступный ходов в Knight.cs

--- Assets/Scripts/Chess/Behaviours/Knight.cs
+++ Assets/Scripts/Chess/Behaviours/Knight.cs
@@ -6,38 +6,37 @@ namespace Assets.Scripts.Chess.Behaviours
 {
     public class Knight : FigureBase
     {
+        private IBoard _board;
+        private bool _isFirstStep = true;
+        
         public override void GetAvalableCellsForMove(IBoard board)
         {
+            if (this._board == null)
+                this._board = board;
+            
             if (InCell == null) return;
 
             AvalableCellsForMove.Clear();
-
-            const int i = 1;
-            const int j = 2;
+           
+            MoveTo(2, 1);
+            MoveTo(2, -1);
+            MoveTo(-2, 1);
+            MoveTo(-2, -1);
             
-            if (InCell.I + j < board.Cells.GetLength(0) && InCell.J + i < board.Cells.GetLength(1))
-                AvalableCellsForMove.Add(board.Cells[InCell.I + j, InCell.J + i]);
+            MoveTo(1, 2);
+            MoveTo(1, -2);
+            MoveTo(-1, 2);
+            MoveTo(-1, -2);
+        }
+        
+        private void MoveTo(int i = 0, int j = 0)
+        {
+            _isFirstStep = false;
 
-            if (InCell.I - j >= 0 && InCell.J + i < board.Cells.GetLength(1))
-                AvalableCellsForMove.Add(board.Cells[InCell.I - j, InCell.J + i]);
-
-            if (InCell.I + j < board.Cells.GetLength(0) && InCell.J - i >= 0)
-                AvalableCellsForMove.Add(board.Cells[InCell.I + j, InCell.J - i]);
-
-            if (InCell.I - j >= 0 && InCell.J - i >= 0)
-                AvalableCellsForMove.Add(board.Cells[InCell.I - j, InCell.J - i]);
-
-            if (InCell.I + i < board.Cells.GetLength(0) && InCell.J + j < board.Cells.GetLength(1))
-                AvalableCellsForMove.Add(board.Cells[InCell.I + i, InCell.J + j]);
-
-            if (InCell.I - i >= 0 && InCell.J + j < board.Cells.GetLength(1))
-                AvalableCellsForMove.Add(board.Cells[InCell.I - i, InCell.J + j]);
-
-            if (InCell.I + i < board.Cells.GetLength(0) && InCell.J - j >= 0)
-                AvalableCellsForMove.Add(board.Cells[InCell.I + i, InCell.J - j]);
-
-            if (InCell.I - i >= 0 && InCell.J - j >= 0)
-                AvalableCellsForMove.Add(board.Cells[InCell.I - i, InCell.J - j]);
+            if (InCell.I + i >= _board.Cells.GetLength(0) || InCell.I + i < 0) return;
+            if (InCell.J + j >= _board.Cells.GetLength(1) || InCell.J + j < 0) return;
+                
+            AvalableCellsForMove.Add(_board.Cells[InCell.I + i, InCell.J + j]);
         }
     }
 }
